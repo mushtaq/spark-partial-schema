6f56c894a803704c34add5e8f04986b2c92dd104
Mushtaq Ahmed <mushtaq.a@gmail.com>
2022-12-02 10:02:18 +0530

    cleanup

--- src/main/scala/example/CorruptFiles.scala
+++ src/main/scala/example/CorruptFiles.scala
@@ -2,7 +2,7 @@ package example
 
 import org.apache.spark.sql.types.StructType
 import org.apache.spark.sql.{SparkSession, functions => f}
-import org.apache.spark.sql.expressions.Window
+import org.apache.spark.sql.expressions.{Window => W}
 
 import scala.util.chaining.scalaUtilChainingOps
 
@@ -18,27 +18,24 @@ object CorruptFiles {
     spark.sparkContext.setLogLevel("ERROR")
 
     val schema = "Item MAP<String, MAP<String, String>>, _corrupt_record String"
+    val json = f.from_json(f.col("value"), StructType.fromDDL(schema))
 
     val exportedDf = spark.read
       .text("src/main/resources/exports")
-      .withColumn("fileName", f.input_file_name())
-      .withColumn("json", f.from_json(f.col("value"), StructType.fromDDL(schema)))
-      .select("json.Item", "json._corrupt_record", "fileName")
+      .withColumn("json", json)
+      .select("json.Item", "json._corrupt_record")
 
     val itemDf = exportedDf.where(f.col("_corrupt_record").isNull)
     val corruptDf = exportedDf.where(f.col("_corrupt_record").isNotNull)
 
-    val orderSpec = Window.orderBy("fileName", "isPrefix")
-    val groupSpec = Window.partitionBy("group")
-
     val fixedDf = corruptDf
-      .withColumn("isPrefix", f.col("_corrupt_record").startsWith("{"))
-      .withColumn("rowNum", f.row_number().over(orderSpec))
+      .withColumn("fileName", f.input_file_name())
+      .withColumn("rowNum", f.row_number().over(W.orderBy("fileName")))
       .withColumn("group", ((f.col("rowNum") - 1) / 2).cast("int"))
-      .withColumn("list", f.collect_list("_corrupt_record").over(groupSpec))
+      .withColumn("list", f.collect_list("_corrupt_record").over(W.partitionBy("group")))
       .withColumn("value", f.array_join(f.col("list"), ""))
+      .withColumn("json", json)
       .where(f.col("rowNum") % 2 === 0)
-      .withColumn("json", f.from_json(f.col("value"), StructType.fromDDL(schema)))
       .select("json.Item")
       .tap(_.show(false))
 
